555d756f866bcc661ef8f541fe21e97f9c8e112b
Lirmortoch <dima2111323@gmail.com>
2025-09-30 17:46:17 +0300

    add 2 new components to upgraded starting project

---
+++ 03-starting-project-upgraded/src/components/CoreConcepts.jsx
@@ -0,0 +1,14 @@
+import CoreConcept from "./CoreConcept";
+import { CORE_CONCEPTS } from "../data-with-examples";
+
+export default function CoreConcepts() {
+    return (
+        <section id='core-concepts'>
+            <h2>Core Concepts!</h2>
+
+            <ul>
+            {CORE_CONCEPTS.map((item) => <CoreConcept key={item.title} {...item} />)}
+            </ul>
+        </section>
+    );
+}
---
+++ 03-starting-project-upgraded/src/components/Examples.jsx
@@ -0,0 +1,39 @@
+import { EXAMPLES } from "../data-with-examples";
+import { useState } from "react";
+import TabButton from "./TabButton";
+
+export default function Examples() {
+    const [selectedTopic, setSelectTopic] = useState();
+      
+    function handleSelect(selectedButton) {
+    setSelectTopic(selectedButton);
+    }
+
+    let tabContent = <p>Please select a topic.</p>;
+    if (selectedTopic) {
+        tabContent = (
+            <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+            </div>
+        );
+    }
+
+    return (
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')} isSelected={selectedTopic === 'components'} >Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')} isSelected={selectedTopic === 'jsx'} >JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')} isSelected={selectedTopic === 'props'} >Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')} isSelected={selectedTopic === 'state'} >State</TabButton>
+          </menu>
+
+          {tabContent}
+          
+        </section>
+    );
+}
