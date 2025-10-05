624441f04ebacac8efa6d1d25c78179d51a63f82
Lirmortoch <dima2111323@gmail.com>
2025-10-05 13:32:28 +0300

    did 07 lesson

--- 03-starting-project-upgraded/src/components/Examples.jsx
+++ 03-starting-project-upgraded/src/components/Examples.jsx
@@ -26,10 +26,10 @@ export default function Examples() {
     return (
         <Section title='Examples' id='examples'>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')} isSelected={selectedTopic === 'components'} >Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')} isSelected={selectedTopic === 'jsx'} >JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')} isSelected={selectedTopic === 'props'} >Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')} isSelected={selectedTopic === 'state'} >State</TabButton>
+            <TabButton onClick={() => handleSelect('components')} isSelected={selectedTopic === 'components'} >Components</TabButton>
+            <TabButton onClick={() => handleSelect('jsx')} isSelected={selectedTopic === 'jsx'} >JSX</TabButton>
+            <TabButton onClick={() => handleSelect('props')} isSelected={selectedTopic === 'props'} >Props</TabButton>
+            <TabButton onClick={() => handleSelect('state')} isSelected={selectedTopic === 'state'} >State</TabButton>
           </menu>
 
           {tabContent}
