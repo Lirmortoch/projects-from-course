38191aa88ed02dc8ffc38955c85bedf5e358edde
Lirmortoch <dima2111323@gmail.com>
2025-10-10 16:00:49 +0300

    added input to change the name

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,14 +3,21 @@ import { useState } from "react"
 export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing]  = useState(false);
 
+    function handleChangeName() {
+        setIsEditing(!isEditing);
+    }
+
+    let playerNameField = <span className="player-name">{name}</span>;
+    if (isEditing) playerNameField = <input type="text" name="name-input" id="name-input" value={name}/>;
+    
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {playerNameField}
               <span className="player-symbol">{symbol}</span>
             </span>
             
-            <button>Edit</button>
+            <button onClick={handleChangeName}>Edit</button>
         </li>
     )
 }
