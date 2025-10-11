c136facf55ce3dac53d6f83bed04b97c95f5fd59
Lirmortoch <dima2111323@gmail.com>
2025-10-11 11:56:27 +0300

    added main functionality to game board(initialize game board and select field), lift active player state up, in the app component (did 22 & 23 lessons)

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectionSquare() {
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName='Joe' symbol='X'/>
-          <Player initialName='Bob' symbol='O'/>
+        <ol id="players" className="highlight-player">
+          <Player initialName='Joe' symbol='X' isActive={activePlayer === 'X'} />
+          <Player initialName='Bob' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectionSquare} activePlayerSymbol={activePlayer} />
 
       </div>
     </main>
