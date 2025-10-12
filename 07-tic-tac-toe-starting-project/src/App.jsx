c5997ca951a39419eef401182e50be8686a5f8ad
Lirmortoch <dima2111323@gmail.com>
2025-10-12 13:06:22 +0300

    Did lesson 25

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -7,11 +7,23 @@ import Log from "./components/Log";
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
-  
 
-  function handleSelectionSquare() {
+
+  function handleSelectionSquare(rowIndex, colIndex) {
     setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
-    setGameTurns();
+    setGameTurns(prevTurns => {
+      let currentPlayer = "X";
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        { square: {row: rowIndex, col: colIndex}, player: currentPlayer }, 
+        ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
