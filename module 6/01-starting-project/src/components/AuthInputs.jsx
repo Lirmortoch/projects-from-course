39de4a94525673c9c2d43c2916a529f44f46e08d
Lirmortoch <dima2111323@gmail.com>
2025-10-22 18:05:46 +0300

    did lesson 11

--- module 6/01-starting-project/src/components/AuthInputs.jsx
+++ module 6/01-starting-project/src/components/AuthInputs.jsx
@@ -14,7 +14,19 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${({ invalid }) => invalid ? '#f87171' : '#6b7280'};
+  color: ${({ $invalid }) => $invalid ? '#f87171' : '#6b7280'};
+`;
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${({ $invalid }) => $invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({ $invalid }) => $invalid ? '#ef4444' : '#374151'};
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${({ $invalid }) => $invalid ? '#f73f3f' : 'transparent'};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
 export default function AuthInputs() {
@@ -41,18 +53,18 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <Label invalid={emailNotValid}>Email</Label>
-          <input
+          <Label $invalid={emailNotValid}>Email</Label>
+          <Input
+            $invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</Label>
-          <input
+          <Label $invalid={passwordNotValid}>Password</Label>
+          <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
