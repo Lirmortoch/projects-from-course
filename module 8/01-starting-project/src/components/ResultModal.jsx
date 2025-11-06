9dce1949b05fd519616cf4a40be876578bb7a796
Lirmortoch <dima2111323@gmail.com>
2025-11-06 20:09:10 +0300

    did lesson 17

--- module 8/01-starting-project/src/components/ResultModal.jsx
+++ module 8/01-starting-project/src/components/ResultModal.jsx
@@ -1,22 +1,25 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
 
 export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
     const dialog = useRef();
 
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
             open() {
                 dialog.current.showModal();
-            }
-        }
+            },
+        };
     });
 
-    return (
+    return createPortal (
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
             </p>
@@ -26,6 +29,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     );
 }
