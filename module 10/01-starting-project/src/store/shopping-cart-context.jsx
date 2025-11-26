04faf865a3afbe14728779ecb9c68bb384b00f0f
Lirmortoch <dima2111323@gmail.com>
2025-11-26 14:16:12 +0300

    did lesson 06

---
+++ module 10/01-starting-project/src/store/shopping-cart-context.jsx
@@ -0,0 +1,7 @@
+import { useReducer, useContext, createContext } from "react";
+import { DUMMY_PRODUCTS } from '../dummy-products.js';
+
+export const CartContext = createContext({
+  items: []
+});
+
