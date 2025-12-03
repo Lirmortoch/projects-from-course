c9025acb3b6ad484eb38a6f01951e9989906c4fa
Lirmortoch <dima2111323@gmail.com>
2025-12-03 17:14:21 +0300

    add some code to Question component, create empty Result component

--- module 12/01-starting-project/src/Components/Question.jsx
+++ module 12/01-starting-project/src/Components/Question.jsx
@@ -1,5 +1,19 @@
 export default function Question({ question, handleSetChoice }) {
-  return {
+  return (
+    <section id="last-try">
+      <h2>{question.text}</h2>
 
-  }
+      <ul>
+        {
+          question.answers.map((answer, idx) => {
+            return (
+              <li key={idx}>
+                <button onClick={() => handleSetChoice(question.id, answer, question['right-answer'])}>{answer}</button>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </section>
+  )
 }
