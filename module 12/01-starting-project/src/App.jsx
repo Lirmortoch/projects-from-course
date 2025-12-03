9fa94e5df8d636061aa9688a2a158d8655d59b6a
Lirmortoch <dima2111323@gmail.com>
2025-12-03 16:34:58 +0300

    add base code for components, add new field right-answer for questions array

--- module 12/01-starting-project/src/App.jsx
+++ module 12/01-starting-project/src/App.jsx
@@ -1,3 +1,17 @@
-function App() {}
+import { useState } from "react";
+import quizLogo from './assets/quiz-logo.png'
+
+function App() {
+  const [selectedQuestions, setSelectedQuestions] = useState([]);
+  
+  return (
+    <main>
+      <header>
+        <img src={quizLogo} alt="Project logo" />
+        <h1>React Quiz</h1>
+      </header>
+    </main>
+  )
+}
 
 export default App;
