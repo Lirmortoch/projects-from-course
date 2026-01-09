3b3b5cdcd92734a575e6ad54640e178c9b5f6835
Lirmortoch <dima2111323@gmail.com>
2026-01-09 14:47:57 +0300

    did lesson 06 (update AvaliablePlaces component)

--- module 16/01-starting-project/src/components/AvailablePlaces.jsx
+++ module 16/01-starting-project/src/components/AvailablePlaces.jsx
@@ -4,42 +4,32 @@ import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
 
 const coords = {
   latitude: 52.35,
   longitude: 4.9166,
 }
 
+// {
+//   const places = await fetchAvailablePlaces();
+//   const sortedPlaces = sortPlacesByDistance(
+//     places,
+//     coords.latitude,
+//     coords.longitude
+//   );
+  
+//   setAvailablePlaces(sortedPlaces);
+//   setIsFetching(false);
+// }
+
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-
-      try {
-        const places = await fetchAvailablePlaces();
-        const sortedPlaces = sortPlacesByDistance(
-          places,
-          coords.latitude,
-          coords.longitude
-        );
-        
-        setAvailablePlaces(sortedPlaces);
-        setIsFetching(false);
-      } catch (error) {
-        setError({
-          message:
-            error.message || 'Could not fetch places, please try again later.',
-        });
-        setIsFetching(false);
-      }
-    }
-
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error, 
+    fetchedData: availablePlaces,
+    setFetchedData: setAvailablePlaces,
+  } = useFetch(fetchAvailablePlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
